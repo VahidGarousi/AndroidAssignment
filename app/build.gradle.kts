9f12abcb4a22ccae949a4d1196a43d468a3008d7
Vahid Garousi <garousiv@gmail.com>
2025-09-04 09:05:27 +0330

    Migrate to `libs.versions.toml` in order to add build-logic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,12 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
-    id("kotlin-parcelize")
-    id("org.jetbrains.kotlin.plugin.compose")
-    id("org.jetbrains.kotlin.plugin.serialization")
-    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -15,7 +14,7 @@ android {
     compileSdk = 36
     defaultConfig {
         applicationId = "ir.miare.androidcodechallenge"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -50,53 +49,29 @@ android {
 }
 
 dependencies {
-    val lifecycle_version = "2.9.3"
-    val jackson_version = "2.19.2"
-    val retrofit_version = "3.0.0"
-    val coroutines_version = "1.10.2"
-    val hilt_version = "2.57.1"
-    val mockFit_version = "2.0.0"
-    val robolectric_version = "4.16"
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.serialization.json)
 
-    implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.fragment:fragment-ktx:1.8.8")
 
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.robolectric:robolectric:$robolectric_version")
-    testImplementation("org.robolectric:shadows-multidex:4.14.1")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
-    androidTestImplementation("androidx.test.ext:junit:1.3.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    implementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.compose.runtime)
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jackson_version")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-joda:$jackson_version")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-json-org:$jackson_version") {
-        exclude(group = "org.json", module = "json")
-    }
-    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
-    implementation("com.squareup.retrofit2:converter-jackson:$retrofit_version")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.1.0")
-    implementation("com.github.javaherisaber.MockFit:runtime:$mockFit_version")
-    kapt("com.github.javaherisaber.MockFit:compiler:$mockFit_version")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines_version")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
-    implementation("com.google.dagger:hilt-android:$hilt_version")
-    kapt("com.google.dagger:hilt-compiler:$hilt_version")
+    implementation(libs.hilt.core)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 
-    implementation(platform("androidx.compose:compose-bom:2025.08.01"))
-    androidTestImplementation(platform("androidx.compose:compose-bom:2025.08.01"))
-
-    implementation("androidx.activity:activity-compose:1.10.1")
-
-    implementation("androidx.compose.runtime:runtime")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.foundation:foundation")
-    implementation("androidx.compose.material3:material3")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    implementation(libs.jackson.kotlin)
+    implementation(libs.jackson.datatype.joda)
+    implementation(libs.jackson.datatype.json.org)
+    implementation(libs.mockfit.runtime)
+    ksp(libs.mockfit.compiler)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id ("com.android.application") version "8.13.0" apply false
-    id ("com.android.library") version "8.13.0" apply false
-    id ("org.jetbrains.kotlin.android") version "2.2.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10"
-    id ("org.jetbrains.kotlin.plugin.serialization") version "2.2.10" apply false
-    id ("com.google.dagger.hilt.android") version "2.57.1" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.compose) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
 }
