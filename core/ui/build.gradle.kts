3fd1e41c5295b52c7be7daecbe7b5c69cbb89e30
Vahid Garousi <garousiv@gmail.com>
2025-09-04 12:13:18 +0330

    Adding core:ui module

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.miare.android.library.compose)
+    alias(libs.plugins.miare.android.library)
+    alias(libs.plugins.miare.hilt)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "ir.miare.core.ui"
+}
+
+
+dependencies {
+    api(projects.core.common)
+//    api(projects.core.designsystem)
+    api(projects.core.domain)
+}
